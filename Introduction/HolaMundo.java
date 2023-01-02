b23ebc848fa544d18ba395670148caadbdfc0839
indiedv <121758322+indiedv@users.noreply.github.com>
2023-01-02 11:55:46 -0500

    Delete Introduction directory

--- Introduction/HolaMundo.java
+++
@@ -1,8 +0,0 @@
-package Introduction;
-
-public class HolaMundo {
-    public static void main(String[]args){
-        System.out.println("Hola Mundo! Java");
-        System.out.println("Hello World! Java");
-    }
-}
